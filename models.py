a2df3e956860983b51f6bacbf5177e788b169819
panoskyr <35936229+panoskyr@users.noreply.github.com>
2023-05-19 13:58:22 +0300

    added nnpredictor

--- models.py
+++ models.py
@@ -86,3 +86,32 @@ class DotProductLinkPredictor(torch.nn.Module):
     
     def reset_parameters(self):
       pass
+
+
+# a simple NN with at least 2 fully connected layers
+class NeuralLinkPredictor(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers,
+                 dropout):
+        super(NeuralLinkPredictor, self).__init__()
+
+        self.lins = torch.nn.ModuleList()
+        self.lins.append(torch.nn.Linear(in_channels, hidden_channels))
+        for _ in range(num_layers - 2):
+            self.lins.append(torch.nn.Linear(hidden_channels, hidden_channels))
+        self.lins.append(torch.nn.Linear(hidden_channels, out_channels))
+
+        self.dropout = dropout
+
+    def reset_parameters(self):
+        for lin in self.lins:
+            lin.reset_parameters()
+
+    def forward(self, x_i, x_j):
+        x = x_i * x_j
+        for lin in self.lins[:-1]:
+            x = lin(x)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.lins[-1](x)
+        print(torch.sigmoid(x).squeeze().shape, torch.sigmoid(x).shape)
+        return torch.sigmoid(x).squeeze()
